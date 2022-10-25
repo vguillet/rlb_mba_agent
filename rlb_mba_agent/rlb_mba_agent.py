94249f9f18acd6e960b9267aee9dddd2e73ecdbf
Victor Guillet <43042800+vguillet@users.noreply.github.com>
2022-10-25 10:47:07 +0200

    update

--- rlb_mba_agent/rlb_mba_agent.py
+++ rlb_mba_agent/rlb_mba_agent.py
@@ -15,6 +15,7 @@ from datetime import datetime
 from functools import partial
 import json
 import time
+import pprint
 
 import sys
 from unittest import result
@@ -25,8 +26,11 @@ from sensor_msgs.msg import JointState, LaserScan
 from rlb_utils.msg import Goal, TeamComm, CommsState
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
-from patrol_messages.msg import BundleDescription, Award, Results, ResultsStamped, Header, AuctionHeader
-
+from caf_messages.msg import (
+    Award, BidStamped, Bid, BundleDescription,
+    ItemDescription, AnnouncementStamped, AuctionHeader,
+    Header, Results, ResultsStamped)
+from caf_essential.utils import utils
 
 import numpy as np
 import pandas as pd
@@ -41,7 +45,7 @@ from rlb_config.simulation_parameters import *
 ##################################################################################################################
 
 
-DEBUG = True
+DEBUG = False
 
 class RLB_mba_agent(
     # Node, 
@@ -80,6 +84,7 @@ class RLB_mba_agent(
         qos = QoSProfile(
             reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_RELIABLE,
             history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_ALL,
+
             )
 
         self.team_comms_publisher = self.create_publisher(
@@ -88,12 +93,28 @@ class RLB_mba_agent(
             qos_profile=qos 
         )
 
-        self.sim_clock_subscriber = self.create_subscriber(
+        qos = QoSProfile(
+            reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+            history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+            depth=1
+            )
+
+        self.sim_clock_subscriber = self.create_subscription(
             msg_type=TeamComm,
             topic="/RLB_clock",
             callback=self.sim_clock_callback,
             qos_profile=qos 
         )
+
+        # ----------------------------------- Goals publisher
+        qos = QoSProfile(depth=10)
+        
+        # Goals publisher
+        self.goal_sequence_publisher = self.create_publisher(
+            msg_type=Goal,
+            topic='/goals_backlog',
+            qos_profile=qos
+            )
 
         # ================================================================= MBA agent
         self.tasks = {}
@@ -137,7 +158,7 @@ class RLB_mba_agent(
 
     # -------------------------------------------------- Auction process
     def sim_clock_callback(self, msg):
-        self.sim_time = (msg.data)
+        self.sim_time = float(msg.data)
 
     def process_msg(self, msg):       
         """
@@ -149,7 +170,6 @@ class RLB_mba_agent(
         Msgs that fail any is not rebroadcased
         """ 
 
-        # self.get_logger().info(f"====================================> self.current_auction_phase: {self.current_auction_phase}")
         if msg.type == "auction_token":
             if self.current_auction_phase == "consensus":
                 self.check_auction_token(msg=msg)
@@ -158,6 +178,7 @@ class RLB_mba_agent(
 
         elif msg.type == "auction_bid":
             if self.current_auction_phase == "biding":
+
                 self.check_auction_bid(msg=msg)
             else:
                 return
@@ -180,20 +201,25 @@ class RLB_mba_agent(
 
     def run_auction_cycle(self):
         if self.current_auction_phase == "consensus":
+            self.get_logger().info(f"{self.robot_id}: Running auction, phase = Consensus")
             self.share_current_auction_token()
 
         elif self.current_auction_phase == "biding":
+            self.get_logger().info(f"{self.robot_id}: Running auction, phase = Biding")
             if self.current_session_id in self.tokens.keys():
-                self.get_logger().info(f"{self.robot_id}: {str(self.current_session_id)[-3:]} - {self.tokens[self.current_session_id]['value']}")
+                self.get_logger().info(f"{self.robot_id}: Session={str(self.current_session_id)[-5:]} - Token value={self.tokens[self.current_session_id]['value']} (task id: {self.tokens[self.current_session_id]['task_id']})")
+                # self.get_logger().info(f"\n{pprint.pformat(self.tokens)}\n")
                 self.share_current_auction_bid()
         
         else:
+            self.get_logger().info(f"{self.robot_id}: Running auction, phase = Award")
             if self.current_session_id in self.tokens.keys():
                 if self.tokens[self.current_session_id]['holder_id'] == self.robot_id:
                     self.share_current_auction_award()
 
     # -------------------------------------------------- Auction award
     def check_auction_award(self, msg):
+        award = json.loads(msg.data)
         session = self.current_session_id
 
         # -> If bid is of current auction
@@ -201,7 +227,6 @@ class RLB_mba_agent(
             if award['holder_id'] == self.robot_id:
                 award = json.loads(msg.data)
                 
-                # TODO: Assign task to robot with id self.robot_id
                 # -> Construct Award msg
                 award_msg = Award()
                 award_msg.bidder_id = self.robot_id
@@ -230,7 +255,31 @@ class RLB_mba_agent(
                 results_stamped_msg.header = header_msg
                 results_stamped_msg.body_msg = results_msg
 
-                pass
+                # -> Assign reward to self
+                self.auction_scheme.result_cb(results_stamped_msg)
+
+                # -> Emit goto goal
+                # -> Create message
+                target_position = json.loads(self.tokens[session]['item_data'])["target_position"]
+                point = Point()
+                point.x = target_position["x"]
+                point.y = target_position["y"]
+                point.z = target_position["z"]
+
+                goal = Goal(
+                    robot_id=self.robot_id,
+                    goal_sequence_id=self.tokens[session]["task_id"],
+                    meta_action="",
+                    priority=0.,
+                    sequence=[point]
+                )
+                
+                if DEBUG: self.get_logger().info(f"\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                if DEBUG: self.get_logger().info(f"++ Goal sequence {goal.goal_sequence_id} emitted: {goal.sequence} for {goal.robot_id} ++")
+                if DEBUG: self.get_logger().info(f"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+                # -> Publish instruction msg to robot
+                self.goal_sequence_publisher.publish(msg=goal)
 
     def share_current_auction_award(self):
         # -> Construct award msg
@@ -240,22 +289,118 @@ class RLB_mba_agent(
         msg.source_type = "robot"
         msg.target = "all"
         msg.type = "auction_award"
-        msg.data = json.dumps(self.create_auction_award())
-        msg.trace = [self.robot_id] 
 
-        self.team_comms_publisher.publish(msg)
+        auction_award = self.create_auction_award()
+
+        if auction_award is not None:
+            msg.data = json.dumps(auction_award)
+            msg.trace = [self.robot_id] 
+
+            self.team_comms_publisher.publish(msg)
 
     def create_auction_award(self):
         session = self.current_session_id
         
         # Create bid msg list
         if not DEBUG:
-            msg.bundle = [self.tokens[session]["task_item"]]
+            if session not in self.auction_bids.keys():
+                self.get_logger().info(f"!!!!!!!!!!! No bids registered for current session {session}")
+                return None
 
-            bids = []
-            best_bidder = self.auction_scheme.solve_wdp(bids)[0]
+            # -> Reconstruct msg from token
+            task_item = ItemDescription()
+            task_item.item_id = str(self.tokens[session]["item_id"])
+            task_item.item_name = self.tokens[session]["item_name"]
+            task_item.item_type = self.tokens[session]["item_type"]
+            task_item.item_data = self.tokens[session]["item_data"]
             
-            # TODO: Run auctioneer self award
+            # Create announcement stamped msg
+            msg = AnnouncementStamped()
+            # fill stamp
+            msg.header = utils.fill_header(self.robot_id)
+            # fill auction header
+            msg.body_msg.auction_header.auction_id = \
+                'auction_'+self.robot_id+'_'+str(
+                    self.auction_cpt)
+            # Define deadline
+            # TODO: remove this if never used
+            # self.auction_duration seconds (int)
+            auction_duration = 0.0  
+            msg.body_msg.auction_deadline = \
+                self.get_clock().now().to_msg()
+            msg.body_msg.auction_deadline.nanosec += int(
+                auction_duration*1e9)
+            msg.body_msg.item_list = [task_item]
+            self.get_logger().info(
+                f"Announcement msg with sensors {msg}")
+            # Store this auction for robot as "auctioneer" role
+            self.auction_scheme.current_auction_auctioneer = {
+                'announcement_st_msg': msg, 'bids_st_msgs': [],
+                'bid_opening': True}
+            self.auction_scheme.auction_awaiting_acknowledgements = \
+                self.auction_scheme.current_auction_auctioneer
+                
+            bids = self.auction_bids[session]
+            best_bidder, best_bid_val, _ = self.auction_scheme.solve_wdp(bids)
+
+            # -> Construct Award msg
+            award_msg = Award()
+            award_msg.bidder_id = best_bidder
+            
+            # -> Construct BundleDescription msg
+            bundle_description_msg = BundleDescription()
+            bundle_description_msg.bundle_id = self.tokens[session]["task_id"]
+
+            bundle_description_msg.item_bundle = [task_item]
+
+            # -> Construct header msg
+            header_msg = Header()
+            header_msg.sender_id = self.robot_id
+            
+            # -> Construct AuctionHeader msg
+            auction_header_msg = AuctionHeader()
+            auction_header_msg.auction_id = str(session)
+
+            # -> Construct Results msg
+            results_msg = Results()
+            results_msg.auction_header = auction_header_msg
+            results_msg.award_list = [award_msg]
+            results_msg.bundle_descriptions = [bundle_description_msg]
+            
+            # -> Construct ResultsStamped msg
+            results_stamped_msg = ResultsStamped()
+            results_stamped_msg.header = header_msg
+            results_stamped_msg.body_msg = results_msg
+            
+            # -> Assign reward to self as auctioneer
+            if best_bidder == self.robot_id:
+                self.auction_scheme.auctioneer_self_award(
+                    results_stamped_msg,
+                    best_bid_val,
+                    self.tokens[session]["task_id"])
+
+                # -> Emit goto goal
+                # -> Create message
+                target_position = json.loads(self.tokens[session]['item_data'])["target_position"]
+                point = Point()
+                point.x = target_position["x"]
+                point.y = target_position["y"]
+                point.z = target_position["z"]
+
+                goal = Goal(
+                    robot_id=self.robot_id,
+                    goal_sequence_id=self.tokens[session]["task_id"],
+                    meta_action="",
+                    priority=0.,
+                    sequence=[point]
+                )
+                
+                if DEBUG: self.get_logger().info(f"\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                if DEBUG: self.get_logger().info(f"++ Goal sequence {goal.goal_sequence_id} emitted: {goal.sequence} for {goal.robot_id} ++")
+                if DEBUG: self.get_logger().info(f"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+                # -> Publish instruction msg to robot
+                self.goal_sequence_publisher.publish(msg=goal)
 
             return {
                 "session": self.current_session_id,
@@ -269,7 +414,7 @@ class RLB_mba_agent(
             }
 
     # -------------------------------------------------- Auction bid
-    def check_auction_bid(self, msg):
+    def check_auction_bid(self, msg):        
         bid = json.loads(msg.data)
         session = self.current_session_id
 
@@ -285,16 +430,46 @@ class RLB_mba_agent(
                 if session not in self.auction_bids.keys():
                     self.auction_bids[session] = []
 
+                    # -> Pop current auctioned task
+                    self.auctioneer_auction_to_start.pop(0)
+                    if DEBUG: self.get_logger().info(f"{self.robot_id}: Length of auctions to start: {len(self.auctioneer_auction_to_start)}")
+
                 # -> Check if current auction entry exists in self.auction_bids
                 if bid not in self.auction_bids[session]:
-                    self.auction_bids[session].append(bid)
+                    # -> Construct header msg
+                    header_msg = Header()
+                    header_msg.sender_id = self.robot_id
+
+                    # -> Construct AuctionHeader msg
+                    auction_header_msg = AuctionHeader()
+                    auction_header_msg.auction_id = str(session)
+
+                    # -> Construct BundleDescription msg
+                    bundle_description_msg = BundleDescription()
+                    bundle_description_msg.bundle_id = bid["task_id"]
+
+                    # -> Create bid msg
+                    bid_msg = Bid()
+                    bid_msg.auction_header = auction_header_msg
+                    bid_msg.bundle_description = bundle_description_msg
+                    bid_msg.bid = [bid["value"]]
+
+                    # -> Created bid stamped msg
+                    bid_stamped_msg = BidStamped()
+                    bid_stamped_msg.header = header_msg
+                    bid_stamped_msg.body_msg = bid_msg
+
+                    self.auction_bids[session].append(bid_stamped_msg)
+                    if DEBUG: self.get_logger().info(f"{self.robot_id}: Registered bid for {session}")
 
                 else:
                     # Bid already registered, pass
+                    if DEBUG: self.get_logger().info(f"{self.robot_id}: Bid already registered, pass")
                     pass
 
             else:
                 # Not auction token holder, pass
+                if DEBUG: self.get_logger().info(f"{self.robot_id}: Not auction token holder, pass")
                 pass
     
     def share_current_auction_bid(self):
@@ -305,29 +480,55 @@ class RLB_mba_agent(
         msg.source_type = "robot"
         msg.target = "all"
         msg.type = "auction_bid"
-        msg.data = json.dumps(self.create_auction_bid())
-        msg.trace = [self.robot_id] 
 
-        self.team_comms_publisher.publish(msg)
+        bid = self.create_auction_bid()
+
+        # self.get_logger().info(f"{self}: ------------------- Check sending out auction bid: {bid}")
+        if bid is not None:
+            # self.get_logger().info(f"{self}: ------------------- Sending out auction bid")
+            msg.data = json.dumps(bid)
+            msg.trace = [self.robot_id] 
+
+            self.team_comms_publisher.publish(msg)
 
     def create_auction_bid(self):
         session = self.current_session_id
 
+        task_item = self.tokens[session]["task_item"]
+
         if not DEBUG:
-            task_value = self.auction_scheme.compute_bid(self.tokens[session]["task_item"])[0]
+            if task_item is False:
+                return None
+
+            # -> Reconstruct msg from token
+            task_item = ItemDescription()
+            task_item.item_id = self.tokens[session]["item_id"]
+            task_item.item_name = self.tokens[session]["item_name"]
+            task_item.item_type = self.tokens[session]["item_type"]
+            task_item.item_data = self.tokens[session]["item_data"]
+
+            task_value = self.auction_scheme.compute_bid(task_item)[0]
+
+            # print(f"{self.robot_id}: task value={task_value}")
 
             return {
                 "session": self.current_session_id,
-                "task_item": self.tokens[session]["task_item"],
+                "task_id": self.tokens[session]["task_id"],
                 "holder_id": self.robot_id,
                 "priority": 0,
-                "value": task_value
+                "value": task_value,
+                                
+                "task_item": True,
+                "item_id": task_item.item_id,
+                "item_name": task_item.item_name,
+                "item_type": task_item.item_type,
+                "item_data": task_item.item_data
             }
 
         else:
             return {
                 "session": self.current_session_id,
-                "task_item": self.tokens[session]["task_item"],
+                "task_item": task_item,
                 "holder_id": self.robot_id,
                 "priority": 0,
                 "value": random.randint(0, 10)
@@ -418,13 +619,21 @@ class RLB_mba_agent(
     def create_auction_token(self, session):
         if not DEBUG:
             task_item, task_id, task_value, task_priority = self.find_task_to_auction(self.current_simulation_time)
+
+            self.get_logger().info(f"==========================================> {self.robot_id}:  Task item to auction: {len(self.auctioneer_auction_to_start)}")
+
             return {
                 "session": session,
-                "task_item": task_item,
                 "task_id": task_id,
                 "holder_id": self.robot_id,
                 "priority": task_priority,
-                "value": task_value
+                "value": task_value,
+                
+                "task_item": task_item is not None,
+                "item_id": task_item.item_id if task_item is not None else None,
+                "item_name": task_item.item_name if task_item is not None else None,
+                "item_type": task_item.item_type if task_item is not None else None,
+                "item_data": task_item.item_data if task_item is not None else None
             }
 
         else:
